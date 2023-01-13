911a470eb74a22071e1f62dd3a49dfc4680588d9
SalvucciFacundo <salvucci1288@gmail.com>
2023-01-12 23:32:49 -0300

    proyecto vacio

---
+++ src/main/java/com/example/backendportafolio/BackEndPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendportafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPortafolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndPortafolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendportafolio/BackEndPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendportafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPortafolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
