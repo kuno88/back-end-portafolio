e61127be571a2274a077506a608b6ba94549070e
SalvucciFacundo <salvucci1288@gmail.com>
2023-01-13 03:04:15 -0300

    actualizando maven

--- src/main/java/com/example/backendportafolio/BackEndPortafolioApplication.java
+++ src/main/java/com/example/backendportafolio/BackEndPortafolioApplication.java
@@ -1,10 +1,8 @@
 package com.example.backendportafolio;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+
 public class BackEndPortafolioApplication {
 
     public static void main(String[] args) {
